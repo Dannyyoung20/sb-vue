213b7e9bb297f7292cce692b08dd3a0c7e0e1519
chuks-o <okpala_chuks@yahoo.com>
2018-02-26 22:22:20 +0100

    Login Component works with Passport

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import VueAxios from 'axios'
+
+// Vue.use(VueAxios)
+window.axios = VueAxios
 
 Vue.use(Vuetify)
 
